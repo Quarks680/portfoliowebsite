9e056163953a6ccb147c40b98b2441805376e0cb
Quarks680 <anuragyou680@gmail.com>
2024-09-02 00:29:00 +0530

    jj

--- src/components/Batmansenserity.jsx
+++ src/components/Batmansenserity.jsx
@@ -6,7 +6,7 @@ function Batmansenserity() {
         <div className="flex flex-col items-center justify-center ">
           <h1 className='text-[15vw] font-bold font-clashdisplay text-anu z-10 uppercase relative'>I am</h1>
           <div className='absolute  z-30'>
-            <img className='h-96 mt-60 w-auto ' src="../public/images/batmanwithoutbakgroundimage.png" alt="" />
+            <img className='h-96 mt-60 w-auto ' src="/images/batmanwithoutbakgroundimage.png" alt="" />
           </div>
           <h1 className="uppercase mt-[50%] text-[12vw] text-[#121212] font-bold font-clashdisplay absolute z-40 tracking-wide  text-outline">vengeance!</h1>
         </div>
